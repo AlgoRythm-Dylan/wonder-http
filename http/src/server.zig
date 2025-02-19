8b0ae152776d731c5871c5a0d0ea3c4217b5c18b
AlgoRythm-Dylan <dperunko@comcast.net>
2025-02-19 15:32:29 -0500

    What if you want to rename server?

--- http/src/server.zig
+++ http/src/server.zig
@@ -3,22 +3,22 @@ const std = @import("std");
 const net = std.net;
 const functions = @import("./serverfunctions.zig");
 
-const Server = @This();
+const Self = @This();
 
 socket: ?net.Server = null,
 port: u16 = 80,
 loopFn: functions.ServerLoopFn = functions.defaultServerLoopFn,
 handleConnectionFn: functions.ServerHandleConnectionFn = functions.defaultServerHandleConnectionFn,
 
-pub fn start(self: *Server) !void {
+pub fn start(self: *Self) !void {
     self.socket = try socklib.createServerSocket(self.port);
     try self.loop();
 }
 
-pub fn loop(self: *Server) !void {
+pub fn loop(self: *Self) !void {
     try self.loopFn(self);
 }
 
-pub fn handleConnection(self: *Server, connection: *net.Server.Connection) !void {
+pub fn handleConnection(self: *Self, connection: *net.Server.Connection) !void {
     try self.handleConnectionFn(self, connection);
 }
